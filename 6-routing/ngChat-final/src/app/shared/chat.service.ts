89d3ef9e2c9171692bba9f25a6637ef086042016
Cassian Lup <cassi.lup@gmail.com>
2018-02-22 18:25:30 +0200

    Adding 6-routing

---
+++ 6-routing/ngChat-final/src/app/shared/chat.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import * as io from 'socket.io-client';
+
+interface Message {
+  text: string;
+};
+
+@Injectable()
+export class ChatService {
+  private url = 'http://localhost:3000';
+  private socket;
+  private messages:Message[] = [];
+  private connection;
+
+  constructor() {
+    this.socket = io(this.url);
+
+    this.connection = this.onMessageReceived().subscribe((message:string) => {
+      this.messages.push({ text: message });
+    });
+  }
+
+  public getMessages():Array<Message> {
+    return this.messages;
+  }
+
+  sendMessage(message){
+    this.socket.emit('chat message', message);
+  }
+
+  onMessageReceived() {
+    let observable = new Observable(observer => {
+      this.socket.on('chat message', (data) => {
+        observer.next(data);
+      });
+      return () => {
+        this.socket.disconnect();
+      };
+    })
+    return observable;
+  }
+}
